449b5bdd95163b7187c891839c88aac0d1442f08
michu2k <michal.strumpf@gmail.com>
2023-03-16 21:15:06 +0100

    .storybook/main.ts: Added Dev commentsd

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,20 +4,20 @@ const config: StorybookConfig = {
   "stories": [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx)" // Load stories from components directory
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y" // Additional addon for accessibility
   ],
   "framework": {
     "name": "@storybook/nextjs",
     "options": {}
   },
   "docs": {
-    "autodocs": true
+    "autodocs": true // Generate docs automatically for each story
   }
 };
 
