790cfeb211e0c2a0d5893a24251ea5def3e51e46
michu2k <michal.strumpf@gmail.com>
2023-03-18 17:51:11 +0100

    Updated preview.ts configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,42 @@
+import {MINIMAL_VIEWPORTS} from "@storybook/addon-viewport";
+
+const customViewports = {
+  iphone7: {
+    name: "iPhone 7",
+    styles: {
+      width: "375px",
+      height: "667px",
+    },
+  },
+  samsungGalaxyS21Ultra: {
+    name: "Samsung Galaxy S21 Ultra",
+    styles: {
+      width: "384px",
+      height: "854px",
+    },
+  },
+};
+
 export const parameters = {
   backgrounds: {
-    default: 'light',
+    default: "light",
+    values: [
+      {name: "light", value: "#ffffff"},
+      {name: "gray", value: "#808080"},
+      {name: "dark", value: "#212b31"}
+    ]
   },
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: "^on[A-Z].*"},
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...customViewports
+    }
+  }
 }
